1433718b97beccf095f2b51d31ab31578c9eb47f
caiommdev <caiomotamarinho22@gmail.com>
2023-11-06 21:47:34 -0300

    removed: all coments

--- src/Payment.java
+++ src/Payment.java
@@ -1,22 +1,18 @@
 public class Payment {
-    // Set up the member variables
     private Customer customer = null;
     private int paymentId = 0;
     private static int paymentCount = 001;
 
     public Payment(Customer cust) {
-// set the member variables
         customer = cust;
         paymentId = paymentCount++;
     }
 
     public void calculateTotalPayment(Hire hire) {
-// call the private method
         issueReceipt(hire);
     }
 
     private void issueReceipt(Hire hire) {
-// print out all the relevant details
         String cust = hire.getCustomer().getName();
         String pCode = hire.getCustomer().getPostcode();
         System.out.println("Printing out receipt for '" + cust + "'....");
